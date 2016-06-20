55957d3b0be4767e47a22fb1fef75edd621344c9
鲁玉兰 <luyulan@luyulandemacbook-pro.local>
2016-06-20 13:37:16 +0800

    Initial Commit

---
+++ YLTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLTableView
+//
+//  Created by 鲁玉兰 on 16/6/20.
+//  Copyright © 2016年 luyulan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLTableView
+//
+//  Created by 鲁玉兰 on 16/6/20.
+//  Copyright © 2016年 luyulan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
